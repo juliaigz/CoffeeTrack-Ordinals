06a6bf304b572bab8df7fd99808cddebbbe8e809
SantiRendon <xantirendonc@gmail.com>
2024-10-19 17:24:41 -0500

    config carbon components and readme.md project

--- src/cafe-ord-frontend/svelte.config.js
+++ src/cafe-ord-frontend/svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { optimizeImports } from "carbon-preprocess-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,13 +9,14 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      pages: 'dist',
-      assets: 'dist',
+      pages: "dist",
+      assets: "dist",
       fallback: undefined,
       precompress: false,
       strict: true,
     }),
   },
+  preprocess: [vitePreprocess(), optimizeImports()],
 };
 
 export default config;
--- src/cafe-ord-frontend/vite.config.js
+++ src/cafe-ord-frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from 'url';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import environment from 'vite-plugin-environment';
-import dotenv from 'dotenv';
+import { fileURLToPath, URL } from "url";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import environment from "vite-plugin-environment";
+import dotenv from "dotenv";
+import { optimizeCss } from "carbon-preprocess-svelte";
 
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: "../../.env" });
 
 export default defineConfig({
   build: {
@@ -16,6 +17,7 @@ export default defineConfig({
         global: "globalThis",
       },
     },
+    // exclude: ["carbon-components-svelte", "carbon-pictograms-svelte"],
   },
   server: {
     proxy: {
@@ -29,14 +31,13 @@ export default defineConfig({
     sveltekit(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
+    optimizeCss(),
   ],
   resolve: {
     alias: [
       {
         find: "declarations",
-        replacement: fileURLToPath(
-          new URL("../declarations", import.meta.url)
-        ),
+        replacement: fileURLToPath(new URL("../declarations", import.meta.url)),
       },
     ],
   },
